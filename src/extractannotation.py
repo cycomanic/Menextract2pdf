6f47e0cfd91994bd458428421951bb22df7d3e97
Jochen Schroeder <cycomanic@mailbox.org>
2016-01-23 21:29:37 +1100

    add notes extraction

--- src/extractannotation.py
+++ src/extractannotation.py
@@ -1,4 +1,4 @@
-# Copyright 2016 Jochen Schröder
+# Copyright 2016 Jochen Schroeder
 #
 # This file is distributed under the terms of the
 # GPLv3 licence. See the COPYING file for details
@@ -19,7 +19,15 @@ HIGHLIGHTSQUERY = """SELECT Files.localUrl, FileHighlightRects.page,
                         ON FileHighlights.fileHash=Files.hash
                     LEFT JOIN FileHighlightRects
                         ON FileHighlightRects.highlightId=FileHighlights.id
-                    WHERE FileHighlightRects.page IS NOT NULL"""
+                    WHERE (FileHighlightRects.page IS NOT NULL)"""
+
+NOTESQUERY = """SELECT Files.localUrl, FileNotes.page,
+                            FileNotes.x, FileNotes.y,
+                            FileNotes.author, FileNotes.note
+                    FROM Files
+                    LEFT JOIN FileNotes
+                        ON FileNotes.fileHash=Files.hash
+                    WHERE FileNotes.page IS NOT NULL"""
 
 global OVERWRITE_PDFS
 OVERWRITE_PDFS = False
@@ -28,31 +36,56 @@ def converturl2abspath(url):
     pth = unquote(str(urlparse(url).path)).decode("utf8") #this is necessary for filenames with unicode strings
     return os.path.abspath(pth)
 
-def parse_query(res):
-    highlights = {}
+def parse_highlights(res, highlights={}):
     for r in res:
         pth = converturl2abspath(r[0])
-        bbox = [[r[2], r[3], r[4], r[5]]]
         pg = r[1]
+        bbox = [[r[2], r[3], r[4], r[5]]]
         if pth in highlights:
-            if pg in highlights[pth]:
-                highlights[pth][pg].append(bbox)
+            if pg in highlights[pth] and 'highlights' in highlights[pth][pg]:
+                highlights[pth][pg]['highlights'].append(bbox)
             else:
-                highlights[pth][pg] = [bbox]
+                highlights[pth][pg] = {'highlights': [bbox]}
         else:
-            highlights[pth] = {pg: [bbox]}
+            highlights[pth] = {pg: {'highlights':[bbox]}}
     return highlights
 
-def highlight_in_document(inpdf, outpdf, coords):
-    for pg in coords.keys():
+def parse_notes(res, notes={}):
+    for r in res:
+        pth = converturl2abspath(r[0])
+        pg = r[1]
+        bbox = [r[2], r[3], r[2]+30, r[3]+30] # needs a rectangle however size does not matter
+        author = r[4]
+        txt = r[5]
+        note = {"rect": bbox, "author": author, "content": txt}
+        if pth in notes:
+            if pg in notes[pth] and 'notes' in notes[pth][pg]:
+                notes[pth][pg]['notes'].append(note)
+            else:
+                notes[pth][pg] = {'notes': [note]}
+        else:
+            notes[pth] = {pg: {'notes':[note]}}
+    return notes
+
+
+def add_annotation2pdf(inpdf, outpdf, annotations):
+    for pg in annotations.keys():
         inpg = inpdf.getPage(pg-1)
-        for crd in coords[pg]:
-            annot = pdfannotation.highlight_annotation(crd)
-            pdfannotation.add_annotation(outpdf, inpg, annot)
+        if 'highlights' in annotations[pg]:
+            print "pg=%d highlight"%pg
+            for hn in annotations[pg]['highlights']:
+                annot = pdfannotation.highlight_annotation(hn)
+                pdfannotation.add_annotation(outpdf, inpg, annot)
+        if 'notes' in annotations[pg]:
+            print "pg=%d note"%pg
+            for nt in annotations[pg]['notes']:
+                print "pg=%d note=%s"%(pg, nt["content"])
+                note = pdfannotation.text_annotation(nt["rect"], contents=nt["content"], author=nt["author"])
+                pdfannotation.add_annotation(outpdf, inpg, note)
         outpdf.addPage(inpg)
     return outpdf
 
-def processpdf(fn, fn_out, coords):
+def processpdf(fn, fn_out, annotations):
     try:
         inpdf = PyPDF2.PdfFileReader(open(fn, 'rb'))
         if inpdf.isEncrypted:
@@ -65,7 +98,7 @@ def processpdf(fn, fn_out, coords):
         print "Could not find pdffile %s"%fn
         return
     outpdf = PyPDF2.PdfFileWriter()
-    outpdf = highlight_in_document(inpdf, outpdf, coords)
+    outpdf = add_annotation2pdf(inpdf, outpdf, annotations)
     if os.path.isfile(fn_out):
         if not OVERWRITE_PDFS:
             print "%s exists skipping"%fn_out
@@ -78,9 +111,10 @@ def processpdf(fn, fn_out, coords):
 
 def mendeley2pdf(fn_db, dir_pdf):
     db = sqlite3.connect(fn_db)
-    highlights = parse_query(db.execute(HIGHLIGHTSQUERY))
-    for fn, locs in highlights.iteritems():
-        processpdf(fn, os.path.join(dir_pdf, os.path.basename(fn)), locs)
+    highlights = parse_highlights(db.execute(HIGHLIGHTSQUERY))
+    annotations_all = parse_notes(db.execute(NOTESQUERY), highlights)
+    for fn, annons in highlights.iteritems():
+        processpdf(fn, os.path.join(dir_pdf, os.path.basename(fn)), annons)
 
 if __name__ == "__main__":
     import argparse
