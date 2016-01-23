5ac8efd9e31b572a9862cd3092f5861f20ecf449
Jochen Schroeder <cycomanic@mailbox.org>
2016-01-24 09:12:07 +1100

    some renaming

--- src/extractannotation.py
+++ src/extractannotation.py
@@ -12,25 +12,8 @@ import PyPDF2
 import warnings
 from dateutil import parser as dtparser
 
-HIGHLIGHTSQUERY = """SELECT Files.localUrl, FileHighlightRects.page,
-                            FileHighlightRects.x1, FileHighlightRects.y1,
-                            FileHighlightRects.x2, FileHighlightRects.y2,
-                            FileHighlights.createdTime
-                    FROM Files
-                    LEFT JOIN FileHighlights
-                        ON FileHighlights.fileHash=Files.hash
-                    LEFT JOIN FileHighlightRects
-                        ON FileHighlightRects.highlightId=FileHighlights.id
-                    WHERE (FileHighlightRects.page IS NOT NULL)"""
 
-NOTESQUERY = """SELECT Files.localUrl, FileNotes.page,
-                            FileNotes.x, FileNotes.y,
-                            FileNotes.author, FileNotes.note,
-                            FileNotes.modifiedTime
-                    FROM Files
-                    LEFT JOIN FileNotes
-                        ON FileNotes.fileHash=Files.hash
-                    WHERE FileNotes.page IS NOT NULL"""
+
 
 global OVERWRITE_PDFS
 OVERWRITE_PDFS = False
@@ -43,24 +26,44 @@ def converturl2abspath(url):
     pth = unquote(str(urlparse(url).path)).decode("utf8") #this is necessary for filenames with unicode strings
     return os.path.abspath(pth)
 
-def parse_highlights(res, highlights={}):
-    for r in res:
+def get_highlights_from_db(db, results={}):
+    query = """SELECT Files.localUrl, FileHighlightRects.page,
+                            FileHighlightRects.x1, FileHighlightRects.y1,
+                            FileHighlightRects.x2, FileHighlightRects.y2,
+                            FileHighlights.createdTime
+                    FROM Files
+                    LEFT JOIN FileHighlights
+                        ON FileHighlights.fileHash=Files.hash
+                    LEFT JOIN FileHighlightRects
+                        ON FileHighlightRects.highlightId=FileHighlights.id
+                    WHERE (FileHighlightRects.page IS NOT NULL)"""
+    ret = db.execute(query)
+    for r in ret:
         pth = converturl2abspath(r[0])
         pg = r[1]
         bbox = [[r[2], r[3], r[4], r[5]]]
         cdate = convert2datetime(r[6])
         hlight = {"rect": bbox, "cdate": cdate}
-        if pth in highlights:
-            if pg in highlights[pth] and 'highlights' in highlights[pth][pg]:
-                highlights[pth][pg]['highlights'].append(hlight)
+        if pth in results:
+            if pg in results[pth] and 'highlights' in results[pth][pg]:
+                results[pth][pg]['highlights'].append(hlight)
             else:
-                highlights[pth][pg] = {'highlights': [hlight]}
+                results[pth][pg] = {'highlights': [hlight]}
         else:
-            highlights[pth] = {pg: {'highlights':[hlight]}}
-    return highlights
+            results[pth] = {pg: {'highlights':[hlight]}}
+    return results
 
-def parse_notes(res, notes={}):
-    for r in res:
+def get_notes_from_db(db, results={}):
+    query = """SELECT Files.localUrl, FileNotes.page,
+                            FileNotes.x, FileNotes.y,
+                            FileNotes.author, FileNotes.note,
+                            FileNotes.modifiedTime
+                    FROM Files
+                    LEFT JOIN FileNotes
+                        ON FileNotes.fileHash=Files.hash
+                    WHERE FileNotes.page IS NOT NULL"""
+    ret = db.execute(query)
+    for r in ret:
         pth = converturl2abspath(r[0])
         pg = r[1]
         bbox = [r[2], r[3], r[2]+30, r[3]+30] # needs a rectangle however size does not matter
@@ -68,15 +71,14 @@ def parse_notes(res, notes={}):
         txt = r[5]
         cdate = convert2datetime(r[6])
         note = {"rect": bbox, "author": author, "content": txt, "cdate":cdate}
-        if pth in notes:
-            if pg in notes[pth] and 'notes' in notes[pth][pg]:
-                notes[pth][pg]['notes'].append(note)
+        if pth in results:
+            if pg in results[pth] and 'notes' in results[pth][pg]:
+                results[pth][pg]['notes'].append(note)
             else:
-                notes[pth][pg] = {'notes': [note]}
+                results[pth][pg] = {'notes': [note]}
         else:
-            notes[pth] = {pg: {'notes':[note]}}
-    return notes
-
+            results[pth] = {pg: {'notes':[note]}}
+    return results
 
 def add_annotation2pdf(inpdf, outpdf, annotations):
     for pg in annotations.keys():
@@ -122,8 +124,8 @@ def processpdf(fn, fn_out, annotations):
 
 def mendeley2pdf(fn_db, dir_pdf):
     db = sqlite3.connect(fn_db)
-    highlights = parse_highlights(db.execute(HIGHLIGHTSQUERY))
-    annotations_all = parse_notes(db.execute(NOTESQUERY), highlights)
+    highlights = get_highlights_from_db(db)
+    annotations_all = get_notes_from_db(db, highlights)
     for fn, annons in highlights.iteritems():
         processpdf(fn, os.path.join(dir_pdf, os.path.basename(fn)), annons)
 
