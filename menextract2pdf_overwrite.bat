b80a5e54855df3872d218367b41bef74a24c6bb4
bwiernik <github@wiernik.org>
2018-08-28 19:56:28 -0400

    Enquote paths for Windows help files

--- menextract2pdf_overwrite.bat
+++ menextract2pdf_overwrite.bat
@@ -2,5 +2,5 @@ REM Helps to find the right mendeley.sqlite-DB
 
 @echo off
 set mendeleydb=
-for /f "delims=" %a in ('dir /b %~1\*www.mendeley.com.sqlite') do @set mendeleydb=%a
-python menextract2pdf.py %mendeleydb% --overwrite
+for /f "delims=" %a in ('dir /b "%1\*www.mendeley.com.sqlite"') do @set mendeleydb=%a
+python menextract2pdf.py "%mendeleydb%" "%~2" --overwrite
